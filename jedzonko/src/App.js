3b4063ec150223d291eab069c92421487162eb5c
Natalia Olas <nataliaolas7@gmail.com>
2020-12-07 14:32:53 +0100

    change

--- jedzonko/src/App.js
+++ jedzonko/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Formularz from './components/FormularzZamowienia/formularzZamowienia';
-import KrokiZamowienia from './components/FormularzZamowienia/krokiZamowienia';
+
 function App() {
   return (
     <p>trololo</p>
