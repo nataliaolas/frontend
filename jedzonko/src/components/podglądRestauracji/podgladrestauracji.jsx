887784585d0074b0c8191275e48ed344f7f8c508
Natalia Olas <nataliaolas7@gmail.com>
2020-12-10 17:11:41 +0100

    comitc2

--- jedzonko/src/components/podglądRestauracji/podgladrestauracji.jsx
+++ jedzonko/src/components/podglądRestauracji/podgladrestauracji.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React,{useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Typography';
+import useStyles from './styles';
+import Rating from '@material-ui/lab/Rating';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 export default function PodgladRestauracji(){
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
-        <Grid align="center">
+        <Container>
+        <Grid align="center" className={classes.gridy}>
         <TextField id="standard-search" label="Wyszukaj restauracje" type="search" />
-        <Button >Default</Button>
+        <Button className={classes.buttonss}>Wyszukaj</Button>
         </Grid>
-    )
+        <Grid item xs={12} className={classes.gridy}>
+            <Typography variant="h4">Nazwa restauracji</Typography>
+            <Rating name="read-only" value={4} readOnly />
+            <Button onClick={handleClickOpen}>Zobacz Opinie</Button>
+            <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Opinie o restauracji:"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+              <Grid>
+          <Rating name="read-only" value={4} readOnly />
+          <Typography>super </Typography>
+          </Grid>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Wyjdź
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </Grid>
+        </Container>
+        
+    );
 }
---
+++ jedzonko/src/components/podglądRestauracji/styles.jsx
@@ -0,0 +1,14 @@
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    buttonss:{
+        top:'50px',
+        right:'70px'
+    },
+    gridy:{
+        margin:theme.spacing(4)
+    }
+}));
+
+
+export default useStyles;
